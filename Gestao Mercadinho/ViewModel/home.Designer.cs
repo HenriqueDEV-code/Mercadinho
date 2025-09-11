376288ccfa8c147c8948faee0a65aff2e9e1d450
HenriqueDEV-code <pobsg.oficialbr@gmail.com>
2025-09-11 10:03:28 -0300

    Adicionar arquivos de projeto.

---
+++ Gestao Mercadinho/ViewModel/home.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Gestao_Mercadinho.ViewModel
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelMenu = new Panel();
+            btnProdutos = new Button();
+            panelLogo = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panelMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Controls.Add(btnProdutos);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 580);
+            panelMenu.TabIndex = 0;
+            // 
+            // btnProdutos
+            // 
+            btnProdutos.BackgroundImageLayout = ImageLayout.None;
+            btnProdutos.Dock = DockStyle.Top;
+            btnProdutos.FlatAppearance.BorderSize = 0;
+            btnProdutos.FlatStyle = FlatStyle.Flat;
+            btnProdutos.ForeColor = Color.White;
+            btnProdutos.Image = Properties.Resources.icons8_comprar_32;
+            btnProdutos.ImageAlign = ContentAlignment.MiddleLeft;
+            btnProdutos.Location = new Point(0, 80);
+            btnProdutos.Name = "btnProdutos";
+            btnProdutos.Padding = new Padding(12, 0, 0, 0);
+            btnProdutos.Size = new Size(220, 63);
+            btnProdutos.TabIndex = 1;
+            btnProdutos.Text = "      PDV";
+            btnProdutos.TextAlign = ContentAlignment.MiddleLeft;
+            btnProdutos.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnProdutos.UseVisualStyleBackColor = true;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(220, 80);
+            panelLogo.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(220, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1157, 80);
+            panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Image = Properties.Resources.icons8_comprar_32;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(0, 143);
+            button1.Name = "button1";
+            button1.Padding = new Padding(12, 0, 0, 0);
+            button1.Size = new Size(220, 63);
+            button1.TabIndex = 2;
+            button1.Text = "      Estoque";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Image = Properties.Resources.icons8_comprar_32;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 206);
+            button2.Name = "button2";
+            button2.Padding = new Padding(12, 0, 0, 0);
+            button2.Size = new Size(220, 63);
+            button2.TabIndex = 3;
+            button2.Text = "      Cadastro de Produtos";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.BackgroundImageLayout = ImageLayout.None;
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Image = Properties.Resources.icons8_comprar_32;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 269);
+            button3.Name = "button3";
+            button3.Padding = new Padding(12, 0, 0, 0);
+            button3.Size = new Size(220, 63);
+            button3.TabIndex = 4;
+            button3.Text = "      Adicionar mais Estoque";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // home
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1377, 580);
+            Controls.Add(panel2);
+            Controls.Add(panelMenu);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "home";
+            Text = "home";
+            panelMenu.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelMenu;
+        private Panel panel2;
+        private Panel panelLogo;
+        private Button btnProdutos;
+        private Button button2;
+        private Button button1;
+        private Button button3;
+    }
+}
